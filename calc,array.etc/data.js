4618caa7f17b393c66ef5ca71ef29bf4b9e706b3
nothingspecial <59864643+linusulve@users.noreply.github.com>
2020-01-14 10:30:01 +0100

    Add files via upload

---
+++ calc,array.etc/data.js
@@ -0,0 +1,16 @@
+module.exports = {
+    posts:
+    [
+        {
+            name: 'thing',
+            url: 'https://google.com/',
+            text: 'text',
+            comments: [
+                {text: 'commentv1'},
+                {text: 'commentv2'},
+                {text: 'commentv3'},
+            ],
+        },
+
+    ],
+};
